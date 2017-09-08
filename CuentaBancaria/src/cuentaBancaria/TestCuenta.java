8ad0e0511b4924e13922fe25b1d2859e63fe2922
leoavalos95 <leonelavalos.95@gmail.com>
2017-09-08 16:27:06 -0300

    ROJO: FALTA METODOS Y ATRIBUTOS

---
+++ CuentaBancaria/src/cuentaBancaria/TestCuenta.java
@@ -0,0 +1,15 @@
+package cuentaBancaria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCuenta {
+
+	@Test
+	public void verSaldoInicial() {
+		Cuenta cuenta = new Cuenta();
+		assertEquals(cuenta.getSaldo(), 0);
+	}
+
+}
